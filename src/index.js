5a9447ea7fe35c04e90aa66c4210c2381c401594
ismailabello <ismailabello1@gmail.com>
2020-07-09 17:31:15 +0100

    Your awesome message

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card.js';
+import Hello from './Hello.js'
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robot } from './robot';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render( 
+ /*   <div>
+    <Card id={robot[0].id} name={robot[0].name} email={robot[0].email}/>
+    <Card id={robot[1].id} name={robot[1].name} email={robot[1].email}/>
+    <Card id={robot[2].id} name={robot[2].name} email={robot[2].email}/>
+    <Card id={robot[3].id} name={robot[3].name} email={robot[3].email}/> 
+    </div>*/
+    <Hello/>, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
